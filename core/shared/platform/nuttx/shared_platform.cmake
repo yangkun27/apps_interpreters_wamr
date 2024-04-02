6742e394f3628dddbb861b883051e70fe355266c
Xin Xu <31881278+xuxin930@users.noreply.github.com>
2024-04-02 08:53:30 +0800

    nuttx: Migrate NuttX CMake build for WAMR (#3256)
    Add NuttX CMake build support for platforms.
    
    Signed-off-by: xuxin19 <xuxin19@xiaomi.com>

--- core/shared/platform/nuttx/shared_platform.cmake
+++ core/shared/platform/nuttx/shared_platform.cmake
@@ -8,11 +8,11 @@ add_definitions(-DBH_PLATFORM_NUTTX)
 include_directories(${PLATFORM_SHARED_DIR})
 include_directories(${PLATFORM_SHARED_DIR}/../include)
 
-include(${PLATFORM_COMMON_POSIX_DIR}/platform_api_posix.cmake)
+include (${CMAKE_CURRENT_LIST_DIR}/../common/posix/platform_api_posix.cmake)
 
-include(${SHARED_DIR}/utils/uncommon/shared_uncommon.cmake)
+include (${SHARED_DIR}/utils/uncommon/shared_uncommon.cmake)
 
-file(GLOB_RECURSE source_all ${PLATFORM_SHARED_DIR}/*.c)
+file (GLOB_RECURSE source_all ${PLATFORM_SHARED_DIR}/*.c)
 
 if(WAMR_BUILD_LIBC_WASI EQUAL 1)
   list(APPEND source_all ${PLATFORM_SHARED_DIR}/../common/posix/posix_file.c)
@@ -22,8 +22,9 @@ if(WAMR_BUILD_LIBC_WASI EQUAL 1)
   set(source_all ${source_all} ${PLATFORM_COMMON_LIBC_UTIL_SOURCE})
 endif ()
 
-include (${CMAKE_CURRENT_LIST_DIR}/../common/memory/platform_api_memory.cmake)
-set (PLATFORM_SHARED_SOURCE ${source_all} ${PLATFORM_COMMON_MATH_SOURCE} ${PLATFORM_COMMON_MEMORY_SOURCE})
+set (PLATFORM_SHARED_SOURCE ${source_all} ${PLATFORM_COMMON_MATH_SOURCE} ${PLATFORM_COMMON_POSIX_SOURCE} ${UNCOMMON_SHARED_SOURCE})
+# remove posix_memmap.c for NuttX
+list(REMOVE_ITEM ${PLATFORM_SHARED_SOURCE} ${PLATFORM_COMMON_POSIX_DIR}/posix_memmap.c)
 
 set(PLATFORM_SHARED_SOURCE
     ${source_all} ${PLATFORM_COMMON_MATH_SOURCE}
