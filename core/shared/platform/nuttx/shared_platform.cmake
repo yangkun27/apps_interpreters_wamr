78af1280795b2f97e02b5ed84a0bcdf7fc5b00f5
xuxin19 <xuxin19@xiaomi.com>
2024-09-09 19:32:54 +0800

    cmake(bugfix):warning add WASM_ENABLE_MODULE_INST_CONTEXT definition
    Change-Id: Ib40d8d19c01da0f84e1f3ddbfccc3b5858dd0f6b

--- core/shared/platform/nuttx/shared_platform.cmake
+++ core/shared/platform/nuttx/shared_platform.cmake
@@ -1,25 +1,32 @@
 # Copyright (C) 2020 XiaoMi Corporation.  All rights reserved.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-set (PLATFORM_SHARED_DIR ${CMAKE_CURRENT_LIST_DIR})
-
+set(PLATFORM_SHARED_DIR ${CMAKE_CURRENT_LIST_DIR})
+set(PLATFORM_COMMON_POSIX_DIR ${CMAKE_CURRENT_LIST_DIR}/../common/posix)
 add_definitions(-DBH_PLATFORM_NUTTX)
 
 include_directories(${PLATFORM_SHARED_DIR})
 include_directories(${PLATFORM_SHARED_DIR}/../include)
 
-include (${CMAKE_CURRENT_LIST_DIR}/../common/posix/platform_api_posix.cmake)
+include(${PLATFORM_COMMON_POSIX_DIR}/platform_api_posix.cmake)
 
-include (${SHARED_DIR}/utils/uncommon/shared_uncommon.cmake)
+include(${SHARED_DIR}/utils/uncommon/shared_uncommon.cmake)
 
-file (GLOB_RECURSE source_all ${PLATFORM_SHARED_DIR}/*.c)
+file(GLOB_RECURSE source_all ${PLATFORM_SHARED_DIR}/*.c)
 
-if (WAMR_BUILD_LIBC_WASI EQUAL 1)
+if(WAMR_BUILD_LIBC_WASI EQUAL 1)
   list(APPEND source_all ${PLATFORM_SHARED_DIR}/../common/posix/posix_file.c)
-  include (${CMAKE_CURRENT_LIST_DIR}/../common/libc-util/platform_common_libc_util.cmake)
+  include(
+    ${CMAKE_CURRENT_LIST_DIR}/../common/libc-util/platform_common_libc_util.cmake
+  )
   set(source_all ${source_all} ${PLATFORM_COMMON_LIBC_UTIL_SOURCE})
-endif ()
+endif()
 
-set (PLATFORM_SHARED_SOURCE ${source_all} ${PLATFORM_COMMON_MATH_SOURCE} ${PLATFORM_COMMON_POSIX_SOURCE} ${UNCOMMON_SHARED_SOURCE})
+set(PLATFORM_SHARED_SOURCE
+    ${source_all} ${PLATFORM_COMMON_MATH_SOURCE}
+    ${PLATFORM_COMMON_POSIX_SOURCE} ${UNCOMMON_SHARED_SOURCE})
 # remove posix_memmap.c for NuttX
-list(REMOVE_ITEM ${PLATFORM_SHARED_SOURCE} ${PLATFORM_COMMON_POSIX_DIR}/posix_memmap.c)
+list(REMOVE_ITEM PLATFORM_SHARED_SOURCE
+     ${PLATFORM_COMMON_POSIX_DIR}/posix_memmap.c)
+list(REMOVE_ITEM PLATFORM_SHARED_SOURCE
+     ${PLATFORM_COMMON_POSIX_DIR}/posix_malloc.c)
